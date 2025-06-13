3e7745a522b32e4630337c35923d495c58f4027d
wwhby <wwhbygx@sina.com>
2025-06-13 12:46:59 +0800

    ref(lifecycle): 子类的生命周期箭头已经完成

--- entry/hvigorfile.ts
+++ entry/hvigorfile.ts
@@ -3,6 +3,18 @@ import { hapTasks } from '@ohos/hvigor-ohos-plugin';
 //1：导入插件包和配置类(直接复制该行代码)
 import { AppRouterPlugin } from "app_router_hvigor_plugin"
 
+// 1、导入
+import { routerRegisterPlugin, PluginConfig } from 'router-register-plugin'
+
+// 2、初始化配置
+const config: PluginConfig = {
+    scanDirs: ['src/main/ets/pages', 'src/main/ets/views'], // 扫描的目录，如果不设置，默认是扫描src/main/ets目录
+    logEnabled: true, // 查看日志
+    viewNodeInfo: false, // 查看节点信息
+    ignoredModules:[], // 忽略的参与构建的模块，根据自己项目自行设置
+    enableUiPreviewBuild: false, // 启用UI预览构建，不建议启动
+}
+
 export default {
     system: hapTasks,  /* Built-in plugin of Hvigor. It cannot be modified. */
     plugins:[
@@ -12,6 +24,7 @@ export default {
             "routerDependencyName":"@zhongrui/app_router",
             //扫描配置@RouterPath装饰器的组件所在的具体路径，可以加快编译速度,不配置scanPackagePath参数，默认路径为："src/main/ets"
             "scanPackagePath": ["src/main/ets"]
-        })
+        }),
+        routerRegisterPlugin(config)
     ]         /* Custom plugin to extend the functionality of Hvigor. */
 }
--- library/src/main/ets/Lifecycle.ts
+++ library/src/main/ets/Lifecycle.ts
@@ -1,40 +1,39 @@
 import hilog from "@ohos.hilog";
 import { LifecycleEventObserver } from "./LifecycleEventObserver";
 import { LifecycleState } from "./LifecycleState";
-import { FrameNode, UIContext, uiObserver } from "@kit.ArkUI";
+import { FrameNode, PageInfo, UIContext, uiObserver } from "@kit.ArkUI";
 
 export function LifecycleOwner(target: any, name: string) {
   if (!target.rerender) {
     hilog.warn(0x0000, 'Lifecycle', '%{public}s', 'LifecycleOwn current target is not a component')
     return
   }
+  let lifecycleInit = function () {
+    let lifecycle = this[name] as Lifecycle
+    createAppear(lifecycle, this)
+    let uiContext: UIContext = this.getUIContext()
+    let uniqueId: number = this.getUniqueId();
+    //没有父节点，说明是Router首页
+    if (this['parent_'] == undefined) {
+      lifecycle.isPage = true
+      lifecycle.isRouterPage = true
+      createRouterPage(lifecycle, uiContext, uniqueId)
+    } else if (this['parent_']['navDestinationId']) { //父节点是NavDestination
+      lifecycle.isPage = true
+      lifecycle.isNavigation = true
+      createNavChange(this['parent_']['navDestinationId'], lifecycle, uiContext)
+      lifecycle['handler'](LifecycleState.ON_WILL_SHOW)
+    }
+  }
   if (target.aboutToAppear) {
     const oldFunction = target.aboutToAppear;
     target.aboutToAppear = function () {
       oldFunction.call(this);
-      let lifecycle = this[name]
-      lifecycle.owner = new WeakRef(this)
-      lifecycle['state'] = LifecycleState.INITIALIZED
-      lifecycle['handler'] = function (nextState: LifecycleState) {
-        this['state'] = nextState;
-        this['mObservers'].forEach((it: LifecycleEventObserver) => {
-          it(lifecycle.owner.deref(), nextState)
-        })
-      }
-      lifecycle['handler'](LifecycleState.ON_APPEAR)
+      lifecycleInit.call(this)
     };
   } else {
     target.aboutToAppear = function () {
-      let lifecycle = this[name]
-      lifecycle.owner = new WeakRef(this)
-      lifecycle['state'] = LifecycleState.INITIALIZED
-      lifecycle['handler'] = function (nextState: LifecycleState) {
-        this['state'] = nextState;
-        this['mObservers'].forEach((it: LifecycleEventObserver) => {
-          it(lifecycle.owner.deref(), nextState)
-        })
-      }
-      lifecycle['handler'](LifecycleState.ON_APPEAR)
+      lifecycleInit.call(this)
     }
   }
   target.lifecycleRegister = function () {
@@ -48,32 +47,14 @@ export function LifecycleOwner(target: any, name: string) {
       delete this[name]
     })
     let parentNode = frameNode?.getParent()
-    if (parentNode == null || parentNode == undefined) { //这个是Router首页
-      lifecycle.isPage = true
+    if (parentNode == null || parentNode == undefined && lifecycle.isPage == false) { //这个是Router首页
       lifecycle.isNavigation = frameNode.getNodeType() == "NavDestination"
-      lifecycle.isRouterPage = !lifecycle.isNavigation
-      if (lifecycle.isRouterPage) {
-        let currentPageInfo = uiContext.getPageInfoByUniqueId(uniqueId).routerPageInfo
-        let pageChange = function (pageInfo: uiObserver.RouterPageInfo) {
-          if (currentPageInfo?.pageId != pageInfo.pageId) { //其他页面不需处理
-            return
-          }
-          if (pageInfo.state == uiObserver.RouterPageState.ON_PAGE_SHOW) {
-            lifecycle['handler'](LifecycleState.ON_SHOWED)
-          } else if (pageInfo.state == uiObserver.RouterPageState.ON_PAGE_HIDE) {
-            lifecycle['handler'](LifecycleState.ON_HIDDEN)
-          } else if (pageInfo.state == uiObserver.RouterPageState.ABOUT_TO_DISAPPEAR) {
-            uiContext.getUIObserver().off('routerPageUpdate', pageChange)
-          }
-        }
-        uiContext.getUIObserver().on('routerPageUpdate', pageChange)
-      } else if (lifecycle.isNavigation) {
-        let currentPageInfo = uiContext.getPageInfoByUniqueId(frameNode.getFirstChild().getUniqueId()).navDestinationInfo
-        uiContext.getUIObserver().on('navDestinationUpdate', createNavChange(currentPageInfo, lifecycle, uiContext))
+      lifecycle.isPage = lifecycle.isNavigation
+      if (lifecycle.isNavigation) {
+        let currentPageInfo =
+          uiContext.getPageInfoByUniqueId(frameNode.getFirstChild().getUniqueId()).navDestinationInfo
+        createNavChange(currentPageInfo?.navDestinationId, lifecycle, uiContext)
       }
-    } else if(parentNode.getNodeType() == 'NavDestination'){
-      let currentPageInfo = uiContext.getPageInfoByUniqueId(frameNode.getUniqueId()).navDestinationInfo
-      uiContext.getUIObserver().on('navDestinationUpdate', createNavChange(currentPageInfo, lifecycle, uiContext))
     }
   }
   if (target.onDidBuild) {
@@ -87,10 +68,39 @@ export function LifecycleOwner(target: any, name: string) {
   }
 }
 
-function createNavChange(currentPageInfo: uiObserver.NavDestinationInfo | undefined, lifecycle: Lifecycle,
+function createAppear(lifecycle: Lifecycle, target: any) {
+  lifecycle.owner = new WeakRef(target)
+  lifecycle['state'] = LifecycleState.INITIALIZED
+  lifecycle['handler'] = function (nextState: LifecycleState) {
+    this['state'] = nextState;
+    this['mObservers'].forEach((it: LifecycleEventObserver) => {
+      it(lifecycle.owner.deref(), nextState)
+    })
+  }
+  lifecycle['handler'](LifecycleState.ON_APPEAR)
+}
+
+function createRouterPage(lifecycle: Lifecycle, uiContext: UIContext, uniqueId: number) {
+  let currentPageInfo = uiContext.getPageInfoByUniqueId(uniqueId).routerPageInfo
+  let pageChange = function (pageInfo: uiObserver.RouterPageInfo) {
+    if (currentPageInfo?.pageId != pageInfo.pageId) { //其他页面不需处理
+      return
+    }
+    if (pageInfo.state == uiObserver.RouterPageState.ON_PAGE_SHOW) {
+      lifecycle['handler'](LifecycleState.ON_SHOWED)
+    } else if (pageInfo.state == uiObserver.RouterPageState.ON_PAGE_HIDE) {
+      lifecycle['handler'](LifecycleState.ON_HIDDEN)
+    } else if (pageInfo.state == uiObserver.RouterPageState.ABOUT_TO_DISAPPEAR) {
+      uiContext.getUIObserver().off('routerPageUpdate', pageChange)
+    }
+  }
+  uiContext.getUIObserver().on('routerPageUpdate', pageChange)
+}
+
+function createNavChange(currentPageId: string, lifecycle: Lifecycle,
   uiContext: UIContext) {
   let pageChange = function (pageInfo: uiObserver.NavDestinationInfo) {
-    if (currentPageInfo?.uniqueId != pageInfo.uniqueId) { //其他页面不需处理
+    if (currentPageId != pageInfo.navDestinationId) { //其他页面不需处理
       return
     }
     if (pageInfo.state == uiObserver.NavDestinationState.ON_SHOWN) {
@@ -109,7 +119,7 @@ function createNavChange(currentPageInfo: uiObserver.NavDestinationInfo | undefi
       uiContext.getUIObserver().off('navDestinationUpdate', pageChange)
     }
   }
-  return pageChange
+  uiContext.getUIObserver().on('navDestinationUpdate', pageChange)
 }
 
 
